06f59d9f90a9cc70c9b26c7af55f39529bde78c0
Tiyana <tiyana.amp@gmail.com>
2025-05-25 15:11:11 +1200

    Update app.py

--- app.py
+++ app.py
@@ -116,8 +116,8 @@ if st.button("🎶 Recommend"):
         predicted_artist_group = "Unknown Artist Group"
 
     st.success(f"✨ As an **{user_mbti}**, you're matched with **{predicted_genre}** music!")
-    st.info(f"🎤 We think you'll enjoy artists such as **{predicted_artist_group}**.")
+    st.warning(f"🎤 We think you'll enjoy artists such as **{predicted_artist_group}**.")
 else:
-  st.error("Click 'Recommend' to get your personalized music suggestion")
+  st.info("Click 'Recommend' to get your personalized music suggestion")
 
 
